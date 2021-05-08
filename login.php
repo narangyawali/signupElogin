d4a4b474e387dda9efa8326ab71a210275b83ee4
naran gyawali <nryng123@gmail.com>
2021-05-08 13:03:41 +0545

    up to sign up

--- login.php
+++ login.php
@@ -1,18 +1,34 @@
-<?php
-include_once 'header.php';
 
-?>
+  <?php
+    include_once 'header.php';
+    
+    ?>
+
 <div class="form">
-
-    <label for="email">EMAIL</label>
+	<form action="login.inc.php" method="POST">
+		<label for="email">EMAIL</label>
    
-    <input type="email" placeholder="email" required>
+    <input type="email" name="email" placeholder="email" required>
     <br>
-    <label for="password">PASSWORD</label>
-	<input type="password" placeholder="password" required>
-    <br>
-    <button type="submit">LOGIN</button>
+    <!-- <label for="password">PASSWORD</label>
+		<input type="password" name="password" placeholder="password" required>
+    <br> -->
+    <button type="submit" name="btn">SEARCH</button>
+    </form>
+    
+    <?php
+    include_once 'header.php';
+    
+    $name = $_GET['name'];
+    $phone = $_GET['phone'];
+    
+    
+    ?>
 
+	<div>
+  	<h1> <?php global $name;echo $name  ?></h1>
+    <h1> <?php global $phone;echo $phone  ?></h1>
+  </div>
 
 </div>
 
